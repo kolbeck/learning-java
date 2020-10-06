9d5f96c572d7d0064ea70349f5b2a4656b35a2c9
Jesper Kolbeck <kolbeckxyz@outlook.com>
2020-10-06 21:35:05 +0200

    Use spring initializr to create base structure

---
+++ src/test/java/xyz/kolbeck/learning/java/LearningJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.kolbeck.learning.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
